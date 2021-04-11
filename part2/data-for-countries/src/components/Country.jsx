f03e190e951d9f94c1604e23bc6ad437fc9b8af0
minhphanhvu <minhphanh.vu@gmail.com>
2021-04-11 03:46:14 -0500

    Finish data-for-countries: add weather API + key

--- part2/data-for-countries/src/components/Country.jsx
+++ part2/data-for-countries/src/components/Country.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 const Country = (props) => {
+  const API_KEY = process.env.REACT_APP_API_KEY;
   const { name, capital, population, languages, flag } = props;
+  const query = 'http://api.weatherstack.com/current?access_key=' + API_KEY + '&query=' + capital;
+  const [ weather, setWeather ] = useState({});
   let id = 0;
+
+  useEffect(() => {
+    axios
+      .get(query).then(response => {
+        const currentWeather = response.data.current;
+        setWeather({
+          temperature: currentWeather.temperature,
+          icon: currentWeather.weather_icons[0],
+          windSpeed: currentWeather.wind_speed,
+          windDir: currentWeather.wind_dir
+        })
+      })
+  }, [])
 
   return (
     <div>
@@ -14,6 +31,10 @@ const Country = (props) => {
         {languages.map(language => <li key={id++}>{language.name}</li>)}
       </ul>
       <img alt="flag" src={flag} width="100px" height="100px" />
+      <h3>Weather in {capital}</h3>
+      <p>temperature: {weather.temperature} Celcius</p>
+      <img alt="weather-icon" src={weather.icon} width="80px" height="80px" />
+      <p><strong>wind: </strong>{weather.windSpeed} mph direction {weather.windDir}</p>
     </div>
   )
 }
