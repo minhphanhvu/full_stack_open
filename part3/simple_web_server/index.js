38a93b3f680024920ba1d7888f533bbf029b6fda
minhphanhvu <minhphanh.vu@gmail.com>
2021-04-12 01:50:40 -0500

    Handling post creating new note

--- part3/simple_web_server/index.js
+++ part3/simple_web_server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
   {
     id: 1,
@@ -21,6 +23,17 @@ let notes = [
     important: true
   }
 ]
+
+// Helpers
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+// RESTful routes
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -45,6 +58,27 @@ app.delete('/api/notes/:id', (request, response) => {
   notes = notes.filter(note => note.id !== id)
 
   response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
 })
 
 const PORT = 3001
