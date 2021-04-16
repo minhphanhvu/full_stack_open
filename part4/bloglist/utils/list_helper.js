3105fff29df5e327e7909f95aab8e30895847a46
minhphanhvu <minhphanh.vu@gmail.com>
2021-04-16 01:21:14 -0500

    Add tests for favoriteBlog + totalLikes

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,29 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((result, blog) => result + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  const compare = (blogA, blogB) => {
+    if (blogA.likes > blogB.likes) {
+      return 1
+    } else if (blogA.likes < blogB.likes) {
+      return -1
+    } else {
+      return 0
+    }
+  }
+
+  return blogs.sort(compare)[blogs.length - 1]
+}
+
+const mostBlogs 
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs
 }
