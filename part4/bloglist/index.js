ec7c75c51149aee38a9ccc4f805c991488fba14a
minhphanhvu <minhphanh.vu@gmail.com>
2021-04-15 19:40:02 -0500

    Refactor logger, middleware

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -2,12 +2,22 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 const cors = require('cors')
 const Blog = require('./models/blog')
 const mongoose = require('mongoose')
 
+logger.info('Connecting to', config.MONGODB_URI)
+
 const mongoUrl = `${config.MONGODB_URI}`
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+        .then(() => {
+          logger.info('connected to MongoDB')
+        })
+        .catch((error) => {
+          logger.error('error connected to MongoDB', error.message)
+        })
 
 app.use(cors())
 app.use(express.json())
@@ -31,5 +41,5 @@ app.post('/api/blogs', (request, response) => {
 })
 
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
