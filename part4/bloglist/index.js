8835c996bce621358e25af861e991c6c1e43c4ec
minhphanhvu <minhphanh.vu@gmail.com>
2021-04-15 19:17:44 -0500

    Refactor the models

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -3,16 +3,8 @@ const express = require('express')
 const app = express()
 const config = require('./utils/config')
 const cors = require('cors')
+const Blog = require('./models/blog')
 const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = `${config.MONGODB_URI}`
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
