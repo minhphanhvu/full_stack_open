c30b778f0a0edc4bc2d3527f5e8c7504faaee785
minhphanhvu <minhphanh.vu@gmail.com>
2021-04-15 20:13:33 -0500

    Refactor app and index.js

---
+++ part4/bloglist/app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blog')
+const cors = require('cors')
+
+// Mongoose and database connection
+const mongoose = require('mongoose')
+
+logger.info('Connecting to', config.MONGODB_URI)
+
+const mongoUrl = `${config.MONGODB_URI}`
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+        .then(() => {
+          logger.info('connected to MongoDB')
+        })
+        .catch((error) => {
+          logger.error('error connected to MongoDB', error.message)
+        })
+
+// Using cors and body-parser
+app.use(cors())
+app.use(express.json())
+
+// Using middleware requestLogger
+app.use(middleware.requestLogger)
+
+// Using router
+app.use('/api/blogs', blogsRouter)
+
+// Using middleware
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,29 +1,10 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
-const blogsRouter = require('./controllers/blog')
-const cors = require('cors')
-const mongoose = require('mongoose')
 
-logger.info('Connecting to', config.MONGODB_URI)
+const server = http.createServer(app)
 
-const mongoUrl = `${config.MONGODB_URI}`
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-        .then(() => {
-          logger.info('connected to MongoDB')
-        })
-        .catch((error) => {
-          logger.error('error connected to MongoDB', error.message)
-        })
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
