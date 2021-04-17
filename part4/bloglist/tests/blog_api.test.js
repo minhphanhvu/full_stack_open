39abf18b274f5089be13544339bfa379850fd7e7
minhphanhvu <minhphanh.vu@gmail.com>
2021-04-16 20:44:54 -0500

    Add test for GET /api/blogs and refactor backend with async/await

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -2,7 +2,25 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
+const helper = require('./blog_helper')
 
 const api = supertest(app)
 
+beforeEach(async () => {
+  await Blog.deleteMany({})
 
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
