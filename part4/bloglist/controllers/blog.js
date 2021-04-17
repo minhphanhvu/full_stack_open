fb2ad2e73760be86f7cc5a4530486686279398b9
minhphanhvu <minhphanh.vu@gmail.com>
2021-04-17 13:23:14 -0500

    Refactor POST /api/blogs/ using async/await

--- part4/bloglist/controllers/blog.js
+++ part4/bloglist/controllers/blog.js
@@ -6,14 +6,10 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response) => {
+  const blog = await new Blog(request.body).save()
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  response.status(201).json(blog)
 })
 
 module.exports = blogsRouter
