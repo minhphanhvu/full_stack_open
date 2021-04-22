84ca950aab4ff5ef4a12c4301a472aa23463d9e7
minhphanhvu <minhphanh.vu@gmail.com>
2021-04-22 07:32:41 -0500

    Add token authentication bearer

--- part4/bloglist/controllers/blog.js
+++ part4/bloglist/controllers/blog.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const { request } = require('express')
 const Blog = require('../models/blog')
 const User = require('../models/user')
@@ -19,10 +20,24 @@ blogsRouter.get('/:id', async (request, response) => {
   }
 })
 
+// Get token from bearer authorization request
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
 
-  const user = await User.findById(body.userId)
+  const user = await User.findById(decodedToken.id)
   const blog = new Blog({
     title: body.title,
     author: body.author,
