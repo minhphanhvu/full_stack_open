046d8f33ac0ad6b69923a4d15eadbc400a8081a8
Minh Vu <minhphanh.vu@gmail.com>
2021-05-13 19:52:11 -0500

    7.8 ultimate hook

---
+++ part7/ultimate-hooks/src/hooks/index.js
@@ -0,0 +1,40 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+export const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(baseUrl)
+      .then(response => setResources(response.data))
+  }, [baseUrl])
+
+  const create = (resource) => {
+    axios
+      .post(baseUrl, resource)
+      .then(response => setResources(resources.concat(response.data)))
+  }
+
+  const service = {
+    create
+  }
+
+  return [
+    resources, service
+  ]
+}
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
